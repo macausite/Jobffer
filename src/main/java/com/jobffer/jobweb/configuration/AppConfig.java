7b8c69067cc9483186a90a9de6c22c8b6646991f
Roberto Ambrosio <roberto@ambrosio.ca>
2016-10-06 11:39:23 +0800

    Added login

--- src/main/java/com/jobffer/jobweb/configuration/AppConfig.java
+++ src/main/java/com/jobffer/jobweb/configuration/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.jobffer.jobweb")
+
 public class AppConfig extends WebMvcConfigurerAdapter{
      
     @Bean
